2ac7b5d0727bacdc1bc4a7f7a8823a0aa3b1ac1a
Pavel Tereschenko <me@stupidcat.wtf>
2018-09-13 04:59:09 +0300

    Update build script

--- src/buildNginxConfig/buildNginxConfig.js
+++ src/buildNginxConfig/buildNginxConfig.js
@@ -1,34 +1,47 @@
 /* @flow */
 
 import fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml';
 
+import kebabCase from 'lodash/kebabCase';
+import set from 'lodash/set';
+
+import { readConfig } from './readConfig';
 import { nginxConfigRender } from './nginxConfigRender';
 
-const nameRegExp = /^proxies\.(json|ya?ml)$/;
-const jsonReqExp = /\.json$/;
+const config = {
+  ...readConfig(process.cwd()),
+};
 
-const proxies = (() => {
-  const dirName = process.cwd();
+const envReg = /^proxy_+([a-z0-9]+)_+([a-z0-9_]+)$/i;
+const proxyPropReg = /^(path|target)$/i;
 
-  const fileName = fs
-    .readdirSync(dirName)
-    .find(currentName => nameRegExp.test(currentName));
+const proxies: $ReadOnlyArray<any> = Object.values(
+  Object.entries(process.env).reduce((acc, [key, value]) => {
+    const res = envReg.exec(key);
 
-  if (!fileName) {
-    console.warn('Proxies list not found');
-    return;
-  }
+    if (!res) return acc;
 
-  const filePath = path.resolve(dirName, fileName);
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+    const [, entryName, propName] = res;
 
-  if (jsonReqExp.test(filePath)) return JSON.parse(fileContent);
+    if (proxyPropReg.test(propName)) {
+      set(acc, [entryName, propName.toLowerCase()], value);
+    } else {
+      set(acc, [entryName, 'headers', kebabCase(propName)], value);
+    }
 
-  return yaml.safeLoad(fileContent);
-})();
+    return acc;
+  }, {}),
+).filter((proxy: any) => proxy.path && proxy.target);
 
-const nginxConfig = nginxConfigRender(proxies);
+const assets = process.env.ASSETS || process.env.assets;
+
+if (assets) config.assets = assets;
+if (proxies.length) config.proxies = proxies;
+
+const nginxConfig = nginxConfigRender(config);
+
+console.log('Current config:', JSON.stringify(config, null, '  '));
+console.log('Generated nginx config:');
+console.log(nginxConfig);
 
 fs.writeFileSync('/etc/nginx/nginx.conf', nginxConfig, 'utf8');
