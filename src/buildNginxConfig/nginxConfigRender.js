92d171566d612fc74595ed87a8c3f238b4ed54c1
Pavel Tereschenko <me@stupidcat.wtf>
2018-09-13 04:58:36 +0300

    Update nginxConfigRender

--- src/buildNginxConfig/nginxConfigRender.js
+++ src/buildNginxConfig/nginxConfigRender.js
@@ -1,67 +1,82 @@
 /* @flow */
 
+import Beautify from 'nginxbeautify';
+
+import * as spaboxConfig from '../spaboxConfig';
+
 import { renderProxies } from './renderProxies';
 
-export const nginxConfigRender = (proxies: mixed) => `
-user  nginx;
-worker_processes  1;
+const beautify = new Beautify({
+  spaces: 2,
+});
 
-error_log  /var/log/nginx/error.log warn;
-pid        /var/run/nginx.pid;
+export const nginxConfigRender = (config: spaboxConfig.Config) =>
+  beautify.parse(`
+    user  nginx;
+    worker_processes  1;
+
+    error_log  /var/log/nginx/error.log warn;
+    pid        /var/run/nginx.pid;
 
 
-events {
-    worker_connections  1024;
-}
-
-
-http {
-    include       /etc/nginx/mime.types;
-    default_type  application/octet-stream;
-
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
-
-    access_log  /var/log/nginx/access.log  main;
-
-    sendfile        on;
-    #tcp_nopush     on;
-
-    keepalive_timeout  65;
-
-    #gzip  on;
-
-    map $http_upgrade $connection_upgrade {
-        default upgrade;
-        '' close;
+    events {
+      worker_connections  1024;
     }
 
-    server {
+
+    http {
+      include       /etc/nginx/mime.types;
+      default_type  application/octet-stream;
+
+      log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                        '$status $body_bytes_sent "$http_referer" '
+                        '"$http_user_agent" "$http_x_forwarded_for"';
+
+      access_log  /var/log/nginx/access.log  main;
+
+      sendfile        on;
+      #tcp_nopush     on;
+
+      keepalive_timeout  65;
+
+      #gzip  on;
+
+      map $http_upgrade $connection_upgrade {
+        default upgrade;
+        '' close;
+      }
+
+      server {
         listen       80;
         server_name  localhost;
 
         charset utf-8;
 
-        root   /app/static;
+        root /var/www;
 
-        location / {
-            add_header Cache-Control "must-revalidate, no-cache, no-store";
-            try_files $uri /index.html;
+        ${
+          (config.proxies || []).find(proxy => proxy.path === '/')
+            ? ''
+            : `
+              location / {
+                add_header Cache-Control "must-revalidate, no-cache, no-store";
+                try_files $uri /index.html;
+              }
+
+              location ${config.assets || '/static'} {
+                expires max;
+              }
+            `
         }
 
-        location /assets {
-            expires max;
-        }
-
-        ${renderProxies(proxies)}
+        ${renderProxies(config.proxies)}
 
         # redirect server error pages to the static page /50x.html
         #
         error_page   500 502 503 504  /50x.html;
         location = /50x.html {
-            root   /usr/share/nginx/html;
+          root /usr/share/nginx/html;
         }
+      }
     }
-}
-`;
+  `);
